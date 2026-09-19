bd463bc83f99880d0541260fd52e2e815ea4ba77
geonardpagaduan14-cyber <geonardpagaduan14@gmail.com>
2026-09-19 13:20:34 +0800

    add my files

---
+++ resources/js/components/app-logo-icon.tsx
@@ -0,0 +1,7 @@
+import type { SVGAttributes } from 'react';
+
+export default function AppLogoIcon(props: SVGAttributes<SVGElement>) {
+    return (
+        <img src="acc_logo-removebg-preview.png" alt="acclogo" />
+    );
+}
